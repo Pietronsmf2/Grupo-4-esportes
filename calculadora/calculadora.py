262bc658d3b4657e8e43d52db368ced1f3d319b2
Pietronsmf2 <pietronsmf@aluno.puc-rio.br>
2026-03-19 14:48:54 +0000

    as

---
+++ calculadora/calculadora.py
@@ -0,0 +1,27 @@
+import soma
+import subtrai
+import multiplica
+import divide
+
+n1 = float(input("Digite o primeiro número: "))
+n2 = float(input("Digite o segundo número: "))
+operador = input("Digite a operação (+, -, *, /): ")
+
+if operador == "+":
+    resultado = soma.somaf(n1, n2)
+    print("Resultado:", resultado)
+
+elif operador == "-":
+    resultado = subtrai.subtraif(n1, n2)
+    print("Resultado:", resultado)
+
+elif operador == "*":
+    resultado = multiplica.multiplicaf(n1, n2)
+    print("Resultado:", resultado)
+
+elif operador == "/":
+    resultado = divide.dividef(n1, n2)
+    print("Resultado:", resultado)
+
+else:
+    print("Operador inválido")
